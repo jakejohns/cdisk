0c58d9a09f79c1df9be27e7c628c0d94c32f5187
jake johns <jake@jakejohns.net>
2018-02-17 03:41:09 -0500

    Completion

---
+++ comp/cdisk.completion.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+_cdisk()
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="create open close list list-open \
+        --config -c --verbose -v"
+
+    case "${prev}" in
+        open)
+            local -r closed=$(comm -23 <(cdisk list) <(cdisk list-open))
+            COMPREPLY=( "$(compgen -W "${closed}" -- "${cur}")" )
+            return 0
+            ;;
+        close)
+            local -r disks=$(cdisk list-open)
+            COMPREPLY=( "$(compgen -W "${disks}" -- "${cur}")" )
+            return 0
+            ;;
+        create)
+            return 0
+            ;;
+        list|open-all|close-all)
+            return 0
+            ;;
+        *)
+        ;;
+    esac
+
+    mapfile -t COMPREPLY < <(compgen -W "${opts}" -- "${cur}")
+   return 0
+}
+
+
+complete -F _cdisk cdisk
+
